f6135bf5022a5b3db2317f2eaa92658bd38a74c1
zynpsnltrk <zynpsnltrk@gmail.com>
2017-07-10 00:41:24 +0300

    Step 10

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def generate_buzz():
     page = '<html><body><h1>'
     page += generator.generate_buzz()
     page += '<h1><p>zeynep</p></h1>' 
+    page += '<h1><p>**********</p></h1>'
     page += '</h1></body></html>'
     return page
 
