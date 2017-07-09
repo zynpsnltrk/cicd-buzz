baec0e31df2168dd246df045cf89fe8cc5baa84f
zynpsnltrk <zynpsnltrk@gmail.com>
2017-07-09 22:33:55 +0300

    Step 5.1

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 def generate_buzz():
     page = '<html><body><h1>'
     page += generator.generate_buzz()
+    page += '<h1><p>zeynep</p></h1>' 
     page += '</h1></body></html>'
     return page
 
